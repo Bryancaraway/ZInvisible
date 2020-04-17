2273c109d4fd4b91e80eed24847980a7ec796a51
Bryan <bcaraway@cern.ch>
2020-04-17 14:37:37 -0500

    fix to datacard production

--- Tools/DeepSleep/makeDataCard_ZHbb.py
+++ Tools/DeepSleep/makeDataCard_ZHbb.py
@@ -28,9 +28,12 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import fun_library as lib
-from fun_library import fill1e, fillne, deltaR, deltaPhi, invM, calc_mtb, getZHbbBaseCuts
+from fun_library import fill1e, fillne, deltaR, deltaPhi, invM, calc_mtb, getZHbbBaseCuts, weighted_quantile
 np.random.seed(1)
 ##
+pt_bins = [200,300] # [200,300,400]
+pt_bin_dict = {'200':'lopt', '300':'hipt'}
+
 
 def sepSigBKgdf(df_,sig_):
     sig_df = pd.DataFrame()
@@ -50,6 +53,11 @@ def sepSigBKgdf(df_,sig_):
                 'Weight':get_weight(cut), 'Name':name}) 
             if isSig:
                 temp_df['HZ_genpt'] = df_[key_]['val']['genZHpt']
+                #for i_,pt_bin in enumerate(pt_bins):
+                #    genptcut = ((temp_df['HZ_genpt'] >= pt_bins[i_]) & (temp_df['HZ_genpt'] < np.inf)) if (i_ == len(pt_bins)-1) else \
+                #               ((temp_df['HZ_genpt'] >= pt_bins[i_]) & (temp_df['HZ_genpt'] < pt_bins[i_+1]))
+                #    temp_df.loc[genptcut,'Name'] = temp_df['Name'][genptcut].copy() + 'gen' + pt_bin_dict[str(pt_bin)]
+                                                                   
             if len(cut) > 0:
                 df = df.append(pd.DataFrame(temp_df[cut]), ignore_index=True)
             else:
@@ -57,13 +65,20 @@ def sepSigBKgdf(df_,sig_):
             return(df)
         #
         if (sig_ in key_):
+            lo      = ((df_[key_]['val']['genZHpt'] >= 200) & (df_[key_]['val']['genZHpt'] < 300))
+            hi      = ((df_[key_]['val']['genZHpt'] >= 300) & (df_[key_]['val']['genZHpt'] < np.inf))
             ttZbb   = (df_[key_]['val']['Zbb'] == True)
             ttHbb   = (df_[key_]['val']['Hbb'] == True)
             ttZqq   = (df_[key_]['val']['Zqq'] == True)
             #
-            sig_df = add_toDF(sig_df,ttZbb,'ttZbb', isSig=True)
-            sig_df = add_toDF(sig_df,ttHbb,'ttHbb', isSig=True)
-            bkg_df = add_toDF(bkg_df,ttZqq,'ttZqq')
+            sig_df = add_toDF(sig_df,cut=ttZbb & lo,       name='ttZgenlopt', isSig=True)
+            sig_df = add_toDF(sig_df,cut=ttZbb & hi,       name='ttZgenhipt', isSig=True)
+            sig_df = add_toDF(sig_df,cut=ttHbb & lo,       name='ttHgenlopt', isSig=True)
+            sig_df = add_toDF(sig_df,cut=ttHbb & hi,       name='ttHgenhipt', isSig=True)
+            sig_df = add_toDF(sig_df,cut=ttZbb & ~lo & ~hi,name='ttZelse',    isSig=True)
+            sig_df = add_toDF(sig_df,cut=ttHbb & ~lo & ~hi,name='ttHelse',    isSig=True)
+            #
+            bkg_df = add_toDF(bkg_df,cut=ttZqq,name='ttZqq')
         else:
             bkg_df = add_toDF(bkg_df)
     #
@@ -80,30 +95,29 @@ def makeDataCard(files_, samples_, outDir_):
     pdata_df = pd.concat([sig_df,bkg_df], ignore_index=True, sort=False)
     del df
     #
-    # Seperate NN histogram by H/Z pt bin
-    nn_bins = np.quantile(sig_df['NN'][sig_df['NN'] > -1],np.arange(0.,1.1,.1))
-    pt_bins = [200,300] # [200,300,400]
-    pt_bin_dict = {'200':'lowpt', '300':'highpt'}
-    def binSigBkg(df_):
-        hist_vals  = []
-        for i,pt_bin in enumerate(pt_bins):
-            if i == len(pt_bins)-1:
-                pt_cut = df_['HZ_pt'] >= pt_bin
-            else:
-                pt_cut = (df_['HZ_pt'] >= pt_bins[i]) & (df_['HZ_pt'] < pt_bins[i+1])
-            #
-            temp_vals,_ = np.histogram(df_['NN'][pt_cut], bins=50, range=(0,1), weights=df_['Weight'][pt_cut])
-            #
-            hist_vals.append(temp_vals)
-        return np.array(hist_vals)
-    #
-    #sig_vals = binSigBkg(sig_df)
-    #bkg_vals  = binSigBkg(bkg_df)
-    #data_vals = sig_vals+bkg_vals
-    #
     # Create a root file to store the shapes for shape fit
     dc_dir = 'Higgs-Combine-Tool'
     root_file = TFile(dc_dir+'/input.root', 'recreate')
+    def setupDC(dc_name, channel):
+        _txt = open(dc_name, 'w')
+        _txt.writelines(['max\t1\n',
+                         'jmax\t'+str(len(set(sig_df['Name']))+len(set(bkg_df['Name']))-1)+'\n', 
+                         'kmax\t*\n',100*'-'+'\n'])
+        _txt.writelines(['shapes * * '+str(root_file.GetName())+' $CHANNEL/$PROCESS $CHANNEL/$PROCESS_$SYSTEMATIC\n', 
+                           100*'-'+'\n',
+                           'bin\t\t\t\t\t'+channel+'\n',
+                           'observation\t\t\t\t'+str(-1)+'\n',
+                           100*'-'+'\n'])
+        b_line      = ['bin\t\t\t\t\t']
+        p_line1     = ['process\t\t\t\t\t']
+        p_line2     = ['process\t\t\t\t\t']
+        r_line      = ['rate\t\t\t\t\t']
+        return _txt, b_line, p_line1, p_line2, r_line
+    #
+    def closeDC(_txt,b_line,p_line1,p_line2,r_line):
+        _txt.writelines([''.join(b_line)+'\n',''.join(p_line1)+'\n',''.join(p_line2)+'\n',''.join(r_line)+'\n',100*'-'+'\n'])
+        _txt.close()
+    #
     for i_,pt_bin in enumerate(pt_bins):
         dif_prefix = 'HZpt_'
         if i_ == len(pt_bins)-1:
@@ -112,31 +126,37 @@ def makeDataCard(files_, samples_, outDir_):
         else:
             pt_cut_str = dif_prefix+str(pt_bins[i_])+'to'+str(pt_bins[i_+1])
             pt_cut = ((OP.ge,pt_bins[i_]),(OP.lt,pt_bins[i_+1]))
+        # Seperate NN histogram by H/Z pt bin
+        quant_cut = ((sig_df['NN'] > -1) &  # only consider quantile contruction for events that qualify for NN discrimination
+                     (pt_cut[0][0](sig_df['HZ_genpt'],pt_cut[0][1])) & 
+                     (pt_cut[1][0](sig_df['HZ_genpt'],pt_cut[1][1])))   
+        quantiles = np.arange(0.,1.1,.1) # break up bins to contain 10% of total MC (might need to change later to account for weights)
+        nn_bins   = weighted_quantile(sig_df['NN']  [quant_cut],quantiles,sig_df['Weight'][quant_cut])
+        mass_bins = weighted_quantile(sig_df['M_sd'][quant_cut],quantiles,sig_df['Weight'][quant_cut]) 
+        bins_dict = {'NN': nn_bins, 'M_sd': mass_bins}
         ## need to add histograms to root file with format $channel/$process_, $channel/$process_$process_systematic
         ## as well as a .txt file in datacard format
-        dir_ = root_file.mkdir(pt_cut_str)
+        channel_name = pt_cut_str
+        dir_ = root_file.mkdir(channel_name)
         dir_.cd()
         #
-        dc_txt = open(dc_dir+'/'+pt_cut_str+'.txt', 'w')
-        dc_txt.writelines(['max\t1\n',
-                           'jmax\t'+str(len(set(sig_df['Name']))+len(set(bkg_df['Name']))-1)+'\n', 
-                           'kmax\t*\n',100*'-'+'\n'])
-        dc_txt.writelines(['shapes * * '+str(root_file.GetName())+' $CHANNEL/$PROCESS $CHANNEL/$PROCESS_$SYSTEMATIC\n', 
-                           100*'-'+'\n',
-                           'bin\t\t\t\t\t'+pt_cut_str+'\n',
-                           'observation\t\t\t\t'+str(-1)+'\n',
-                           100*'-'+'\n'])
-        bin_line      = ['bin\t\t\t\t\t']
-        process_line1 = ['process\t\t\t\t\t']
-        process_line2 = ['process\t\t\t\t\t']
-        rate_line     = ['rate\t\t\t\t\t']
+        dc_txt, bin_line, process_line1, process_line2, rate_line = setupDC(dc_name=dc_dir+'/'+channel_name+'.txt', channel=channel_name)
         #
-        def fillHistDC(df_,issig=False,ispData=False):
-            bins_min_max = (10,nn_bins)
+        def fillHistDC(df_, issig=False,ispData=False):
+            bins_min_max = ((len(bins_dict['NN'])-1)*(len(bins_dict['M_sd'])-1),0,(len(bins_dict['NN'])-1)*(len(bins_dict['M_sd'])-1))
+            def getflat2dHist(df_):
+                pt_cut_apl = (pt_cut[0][0](df_['HZ_pt'],pt_cut[0][1])) & (pt_cut[1][0](df_['HZ_pt'],pt_cut[1][1]))
+                #
+                h_bins, _x, _y = np.histogram2d(x=df_['NN'][pt_cut_apl], y=df_['M_sd'][pt_cut_apl], # fill 2d histogram of mass (y) vs NN score (x) 
+                                                bins=[bins_dict['NN'],bins_dict['M_sd']],
+                                                weights=df_['Weight'][pt_cut_apl]) 
+                return h_bins.flatten()
+            #
             if ispData:
                 h_data = TH1F('data_obs','data_obs',*bins_min_max)
-                pt_cut_apl = (pt_cut[0][0](df_['HZ_pt'],pt_cut[0][1])) & (pt_cut[1][0](df_['HZ_pt'],pt_cut[1][1]))
-                [h_data.Fill(entry,w) for entry, w in zip(df_['NN'][pt_cut_apl], df_['Weight'][pt_cut_apl])]
+                #[h_data.Fill(entry,w) for entry, w in zip(df_[kinem][pt_cut_apl], df_['Weight'][pt_cut_apl])]
+                h_bins = getflat2dHist(df_)
+                [h_data.SetBinContent(i,entry) for i,entry in enumerate(h_bins)]
                 root_file.Write()
                 del h_data
                 return
@@ -144,20 +164,21 @@ def makeDataCard(files_, samples_, outDir_):
             h_arr = []
             for j_,sample_ in enumerate(set(df_['Name'])):
                 sample_df = df_[df_['Name'] == sample_]
-                pt_cut_apl = (pt_cut[0][0](sample_df['HZ_pt'],pt_cut[0][1])) & (pt_cut[1][0](sample_df['HZ_pt'],pt_cut[1][1]))
                 #
-                if issig:
-                    sample_ = sample_+'_'+pt_bin_dict[str(pt_bin)]
-                    pt_cut_apl = (pt_cut[0][0](sample_df['HZ_genpt'],pt_cut[0][1])) & (pt_cut[1][0](sample_df['HZ_genpt'],pt_cut[1][1]))
+                #if issig:
+                #    sample_ = sample_+'_'+pt_bin_dict[str(pt_bin)]
                 h_tuple = (sample_,sample_,*bins_min_max)
                 h_temp  = TH1F(*h_tuple)
-                [h_temp.Fill(entry, w) for entry, w in zip(sample_df['NN'][pt_cut_apl], sample_df['Weight'][pt_cut_apl])]
+                #[h_temp.Fill(entry, w) for entry, w in zip(sample_df[kinem][pt_cut_apl], sample_df['Weight'][pt_cut_apl])]
+                h_bins = getflat2dHist(sample_df)
+                [h_temp.SetBinContent(i,entry) for i,entry in enumerate(h_bins)]
                 #
+                nonlocal channel_name
                 nonlocal bin_line
                 nonlocal process_line1
                 nonlocal process_line2
                 nonlocal rate_line
-                bin_line          = bin_line +  [pt_cut_str+'\t']
+                bin_line          = bin_line +  [channel_name+'\t']
                 process_line1     = process_line1 + ['{:14}\t'.format(sample_)]
                 if issig:
                     process_line2 = process_line2 + [str(j_+1-len(set(df_['Name'])))+'\t\t']
@@ -173,15 +194,12 @@ def makeDataCard(files_, samples_, outDir_):
             root_file.Write()
             del h_arr
         #
-        fillHistDC(sig_df,issig=True)
+        fillHistDC(sig_df,  issig=True)
         fillHistDC(bkg_df)
         fillHistDC(pdata_df,ispData=True)
         #
-        dc_txt.writelines([''.join(bin_line)+'\n',''.join(process_line1)+'\n',''.join(process_line2)+'\n',''.join(rate_line)+'\n',100*'-'+'\n'])
-        dc_txt.close()
-        #
+        closeDC(dc_txt, bin_line, process_line1, process_line2, rate_line)
     #
-
     root_file.Close()
     
 if __name__ == '__main__':
